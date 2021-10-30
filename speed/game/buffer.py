a09b14dd23027ab2d59abdd96a66846d42d4279a
tatendafmukaro <tatendafmukaro@outlook.com>
2021-10-30 04:37:46 +0200

    bufferclassskeleton | actor-classfromsnake

---
+++ speed/game/buffer.py
@@ -0,0 +1,12 @@
+# Buffer class to be updated
+class Buffer:
+       """
+       Handles the buffer
+
+    Stereotype: 
+        Service Provider
+
+    Attributes:
+        _words: keeps track of the words list
+    """
+    pass
