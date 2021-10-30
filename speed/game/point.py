b0ec9dba31149c70e46dbddd544bc956a847cd5e
Alan Crisanto <alanvcrisanto@gmail.com>
2021-10-30 11:35:51 -0500

    comments to class

--- speed/game/point.py
+++ speed/game/point.py
@@ -10,20 +10,38 @@ class Point:
         Attributes:
             _x (integer): The horizontal distance.
             _y (Point): The vertical distance.
-    """
+        """
         self._x = x
         self._y = y
 
         def get_x(self):
 
+            """Gets the horizontal distance.
+        
+            Args:
+                self (Point): An instance of Point.
+                
+            Returns:
+                integer: The horizontal distance.
+            """
+
             return self._x
 
         def get_y(self):
+            
+            """Gets the vertical distance.
+        
+            Args:
+                self (Point): An instance of Point.
+                
+            Returns:
+                integer: The vertical distance.
+            """
 
             return self._y
 
         def add(self, other):
-            
+
             """Gets a new point that is the sum of this and the given one.
 
             Args:
